8e7161dcefa3e026d72b353def0792c5a932e313
vamsi krishna <r18cs273@cit.reva.edu.in>
2024-05-29 19:16:43 +0530

    User entity

---
+++ src/main/java/com/train_management/ticket_service/model/User.java
@@ -0,0 +1,55 @@
+package com.train_management.ticket_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String emailAddress;
+
+    private String firstName;
+
+    private String lastName;
+
+    public User(String emailAddress, String firstName, String lastName) {
+        this.emailAddress = emailAddress;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
